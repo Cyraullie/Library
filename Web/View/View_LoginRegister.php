9ddc685a2606e5a10fd5b354ce93ae82d6c68088
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2020-11-04 15:38:37 +0100

    Finish login/register page

--- Web/View/View_LoginRegister.php
+++ Web/View/View_LoginRegister.php
@@ -12,9 +12,46 @@
             <div class="row">
                 <div class="column">
                     <h1>Login</h1>
+                    <form method="post" action="/">
+                        <table class="textLogin">
+                            <tr class="textLogin">
+                                <td><input type="text" class="textLogin" placeholder="Email"></td>
+                            </tr>
+                            <tr class="textLogin">
+                                <td><input type="text" class="textLogin" placeholder="Mot de passe"></td>
+                            </tr>
+                            <tr>
+                                <td><input type="submit" class="textLogin Button" value="Se connecter"></td>
+                            </tr>
+                        </table>
+                    </form>
                 </div>
                 <div class="column">
                     <h1>Register</h1>
+                    <form method="post" action="/">
+                        <table class="textLogin">
+                            <tr class="textLogin">
+                                <td><input type="text" class="textLogin" placeholder="Nom"></td>
+                                <td><input type="text" class="textLogin" placeholder="Prénom"></td>
+                            </tr>
+                        </table>
+                        <table class="textLogin">
+                            <tr class="textLogin">
+                                <td><input type="text" class="textLogin" placeholder="Pseudo"></td>
+                                <td><input type="text" class="textLogin" placeholder="Email"></td>
+                            </tr>
+                            <tr class="textLogin">
+                                <td><input type="text" class="textLogin" placeholder="Téléphone"></td>
+                            </tr>
+                            <tr class="textLogin">
+                                <td><input type="text" class="textLogin" placeholder="Mot de passe"></td>
+                                <td><input type="text" class="textLogin" placeholder="Confirmer le mot de passe"></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><input type="submit" class="textLogin Button" value="S'inscrire"></td>
+                            </tr>
+                        </table>
+                    </form>
                 </div>
             </div>
         </main>
