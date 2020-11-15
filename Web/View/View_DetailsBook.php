3f66922c247a3e143e434dc061229c44c2db0018
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2020-11-15 01:06:11 +0100

    Finish details page

--- Web/View/View_DetailsBook.php
+++ Web/View/View_DetailsBook.php
@@ -7,5 +7,38 @@
 ?>
 
 <html lang="fr">
+    <body class="Page">
+<h1 align="center">Détails du livre</h1>
+<main class="BasePage">
+    <div class="rowBook">
+        <div class="imageBook">
+            <img class="ImageBookDetail" src="Assets/image/Livre1.jpg">
+        </div>
 
+        <div class="columnBook">
+            <form class="DetailsBook" method="post" action="/">
+                <div class="DataBookDetails">
+                    <div class="InfoBook">
+                        <h1>L'invocation : Le Novice</h1>
+                        <p><b>Tome 1</b></p>
+                        <p>Écrivain: <b>Taran Matharu</b></p>
+                        <p>Éditeur: <b>Hachette</b></p>
+                        <p>Propriétaire: <b>Cyril Goldenschue</b></p>
+                    </div>
+
+
+                </div>
+                    <div class="SummaryDetailsBook">
+                        <div class="summary">
+                            <p><b>résumé : </b></p>
+                            Orphelin, Fletcher imagine déjà le futur avec une vie assez difficile, mais dans la forge de son père adoptif, qui l'a pris sous son aile dès qu'il a été retrouvé seul, abandonné.
+                        </div>
+                    </div>
+                    <input type="submit" class="ButtonBook" value="Ajouter">
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+</body>
 </html>
--- Web/View/View_LoanPage.php
+++ Web/View/View_LoanPage.php
@@ -1,1 +1,44 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Cyril.GOLDENSCHUE
+ * Date: 14/11/2020
+ */
+?>
+
+<html>
+<body class="Page">
+<main class="BasePage">
+    <div class="rowBook">
+        <div class="columnLogin">
+            <h1>Login</h1>
+            <form class="textLogin"  method="post" action="/">
+                <input type="text" class="textLogin" placeholder="Email">
+                <input type="text" class="textLogin" placeholder="Mot de passe">
+                <input type="submit" class="textLogin Button" value="Se connecter">
+            </form>
+
+        </div>
+        <div class="columnLogin">
+            <h1>Register</h1>
+            <form class="textLogin" method="post" action="/">
+
+                <input type="text" class="textLogin" placeholder="Nom">
+                <input type="text" class="textLogin" placeholder="Prénom">
+
+                <input type="text" class="textLogin" placeholder="Pseudo">
+                <input type="text" class="textLogin" placeholder="Email">
+
+                <input type="text" class="textLogin" placeholder="Téléphone">
+
+                <input type="text" class="textLogin" placeholder="Mot de passe">
+                <input type="text" class="textLogin" placeholder="Confirmer le mot de passe">
+
+                <input type="submit" class="textLogin Button" value="S'inscrire">
+
+            </form>
+        </div>
+    </div>
+</main>
+</body>
+</html>
