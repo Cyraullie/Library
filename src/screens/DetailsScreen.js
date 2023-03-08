6371612e2adbae28256ba53e4d377e3fdf6b367b
Cyril Goldenschue <cyril.goldenschue@emploilausanne.ch>
2023-03-08 10:06:06 +0100

    Create detailsScreen

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,37 @@
+import T1 from '../assets/img/TMIT1.jpg';
+
+function DetailScreen() {
+    return (
+      <div className='Page'>
+          <form className="bookLoanArea">
+              <div className="bookAreaLoanImage">
+                <img  className="bookLoanImage" src={T1}></img>
+              </div>
+              <div className="bookLoanAreaText">
+                  <div class="dataLoan">
+                    <div className="bookAddTextData">
+                        <h1>L'invocateur : Le Novice <b>T. 1</b></h1>
+                    </div>
+                        <p>Écrivain: <b>Taran Matharu</b></p>
+                        <p>Éditeur: <b>Hachette</b></p>
+                        <p>Propriétaire: <b>Cyril Goldenschue</b></p>
+                  </div>  
+                  
+                  
+
+                  <p><b>Résume : </b></p>
+                    <div class="resumeLoan">
+                        Orphelin, Fletcher imagine déjà le futur avec une vie assez difficile, mais dans la forge de son père adoptif, qui l'a pris sous son aile dès qu'il a été retrouvé seul, abandonné.
+                    </div>
+              
+                  <input type="submit" className="bookButton" value="Emprunter"></input>
+              </div>
+          </form>
+      </div>
+    );
+  }
+  
+  export default DetailScreen;
+  
+  
+  
