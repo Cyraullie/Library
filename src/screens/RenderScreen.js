ea80535082e561393bf72a8543e08d82c462b030
Cyril Goldenschue <cyril.goldenschue@emploilausanne.ch>
2023-03-08 15:26:10 +0100

    Add button to render book

--- src/screens/RenderScreen.js
+++ src/screens/RenderScreen.js
@@ -11,6 +11,7 @@ function RenderScreen() {
               <th className='bookCover'>Livre</th>
               <th>Informations</th>
               <th className='bookState'>état</th>
+              <th className='bookCheck'>Cocher</th>
             </tr>
           </thead>
             
@@ -19,19 +20,27 @@ function RenderScreen() {
               <td className='bookCover'><img className="LoanBookImage" src={T1}></img></td>
               <td>L'invocateur T. 1, Taran Matharu</td>
               <td className='bookState'>en stock</td>
+              <td className='bookCheck'><input type="checkbox" /></td>
             </tr>
             <tr>
               <td className='bookCover'><img className="LoanBookImage" src={T2}></img></td>
               <td>L'invocateur T. 2, Taran Matharu</td>
               <td className='bookState'>en stock</td>
+              <td className='bookCheck'><input type="checkbox" /></td>
             </tr>
             <tr>
               <td className='bookCover'><img className="LoanBookImage" src={T3}></img></td>
               <td>L'invocateur T. 3, Taran Matharu</td>
               <td className='bookState'>en stock</td>
+              <td className='bookCheck'><input type="checkbox" /></td>
             </tr>
           </tbody>
         </table>
+
+        <div className='renderButtons'>
+          <a>Annuler</a>
+          <a>Rendre</a>
+        </div>
       </div>
     );
   }
