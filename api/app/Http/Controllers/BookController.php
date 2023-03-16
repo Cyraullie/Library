e953b716929be7189f67e799413e2e70727c327d
Cyril Goldenschue <cyril.goldenschue@emploilausanne.ch>
2023-03-16 14:56:07 +0100

    Add book_listing on bookController

--- api/app/Http/Controllers/BookController.php
+++ api/app/Http/Controllers/BookController.php
@@ -9,8 +9,30 @@ class BookController extends Controller
 {
     public function book_listing(){
         $books = Book::all();
-        return print_r($books);
+
+        $books_array = [];
+
+        foreach ($books as $book){
+            array_push($books_array, [
+                "id" => $book->id,
+                "name" => $book->name,
+                "volume" => $book->volume,
+                "author" => $book->author,
+                "editor" => $book->editor,
+                "price" => $book->price,
+                "type" => $book->type,
+                "img_name" => $book->img_name,
+                "add_date" => $book->add_date,
+                "summary" => $book->summary,
+                "user_lastname" => $book->user->lastname,
+                "user_firstname" => $book->user->firstname,
+            ]);
+        };
+
+
+        return $books_array;
     }
+
 
     public function book_first(){
         $book = Book::first();
